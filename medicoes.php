5b93112fc5ac5a4fd5541bf757fab3f13796c5b3
calengafernando <fernandot.calenga@gmail.com>
2026-05-28 08:58:58 +0100

    arquivo que irá fornecer o historico de registros para a aplicacao flutter

---
+++ medicoes.php
@@ -0,0 +1,22 @@
+<?php
+    require 'db.php';
+    header ('Content-Type: application/json');
+
+    //Arquivo que ira fornecer o historico de medições
+    //start
+    try{
+        $stmt = $pdo -> query("
+        SELECT id_medicao, registado_em, ph, turbidez, temperatura, resultado, latitude, longitude
+        FROM medicao
+        ORDER BY registado_em DESC
+        LIMIT 100
+        ");
+
+        $medicoes = $stmt -> fetchAll(PDO::FETCH_ASSOC);
+
+        echo json_encode($medicoes);
+    } catch (PDOException $e){
+        http_response_code(500);
+        echo json_encode(['erro' => 'Erro ao obter medições']);
+    }
+?>
