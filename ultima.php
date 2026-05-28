3a50f85e2ec5ff7f662fac2638391dbe51330234
calengafernando <fernandot.calenga@gmail.com>
2026-05-28 10:07:53 +0100

    arquivo de leitura atual da agua

---
+++ ultima.php
@@ -0,0 +1,26 @@
+<?php 
+    require 'db.php';
+    header('Content-Type: application/json');
+
+    try{
+        $stmt = $pdo -> query("
+            SELECT id_medicao, registado_em, ph, turbidez, temperatura, resultado, latitude; longitude
+            FROM medicao
+            ORDER BY registado_em DESC
+            LIMIT1 
+        ");
+
+        $ultima = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if (!$ultima) {
+        http_response_code(404);
+        echo json_encode(['erro' => 'Nenhuma medição encontrada']);
+        exit;
+        }
+
+        echo json_encode($ultima);
+    }catch (PDOException $e){
+        http_response_code(500);
+        echo json_encode(['erro' => 'Erro ao obter última medição']);
+    }
+?>
