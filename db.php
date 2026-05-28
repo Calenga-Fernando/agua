d5a5df4b3052cb67aa3ec1071de19a4371b38f9e
calengafernando <fernandot.calenga@gmail.com>
2026-05-28 14:03:17 +0100

    d

--- db.php
+++ db.php
@@ -6,13 +6,17 @@ $user     = getenv('PGUSER');
 $password = getenv('PGPASSWORD');
 
 try {
-    $pdo = new PDO(
-    "pgsql:host=$host;port=$port;dbname=$dbname;sslmode=require",
+    $dsn = "pgsql:host=$host;port=$port;dbname=$dbname;sslmode=require";
+
+$pdo = new PDO(
+    $dsn,
     $user,
     $password,
-    [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
+    [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_TIMEOUT => 10
+    ]
 );
-
 } catch (PDOException $e) {
     http_response_code(500);
     echo json_encode(['erro' => 'Falha na ligação à BD']);
