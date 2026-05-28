0b7284544373d999bc768808acc69b9d5fcde9c3
calengafernando <fernandot.calenga@gmail.com>
2026-05-28 08:16:48 +0100

    der, modelo logico e script sql de base de dados.

--- docs/db.sql
+++ docs/db.sql
@@ -1,22 +1,11 @@
-/* Projecto de automação */
-
-CREATE TABLE agua (
-    id_agua INT PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR (255) UNIQUE NOT NULL
-)ENGINE=InnoDB;
-
 CREATE TABLE medicao (
-    turbidez FLOAT NOT NULL,
-    latitude DECIMAL(9,6),
-    ph FLOAT NOT NULL,
-    temperatura FLOAT NOT NULL,
-    longitude DECIMAL(9, 6),
-    localizacao VARCHAR (255),
-    id_medicao INT PRIMARY KEY auto_increment,
-    registado_em DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    id_agua INT NOT NULL,
-
-    FOREIGN KEY (id_agua)
-      REFERENCES agua (id_agua)
-      ON DELETE RESTRICT
-)ENGINE=InnoDB;
+    id_medicao   INT          PRIMARY KEY AUTO_INCREMENT,
+    registado_em DATETIME     DEFAULT CURRENT_TIMESTAMP,
+    ph           FLOAT        NOT NULL,
+    turbidez     FLOAT        NOT NULL,
+    temperatura  FLOAT        NOT NULL,
+    latitude     DECIMAL(9,6),
+    longitude    DECIMAL(9,6),
+    resultado    ENUM('potavel','nao_potavel') NOT NULL,
+    localizacao  VARCHAR(255)
+) ENGINE=InnoDB;
